6f09e5c67ca6b7a1b1004d6f25dc91dfe529e8fa
Mikoo Saguindang <mikoo@getmercuryapp.com>
2019-09-25 17:23:50 +0800

    Add home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginModule } from './login/login.module';
 import { CommonModule } from '@angular/common';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { CommonModule } from '@angular/common';
     imports: [
         BrowserModule,
         LoginModule,
+        HomeModule,
         AppRoutingModule,
         CommonModule
     ],
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [LoginComponent],
     imports: [
+        CommonModule,
         LoginRoutingModule
     ]
 })
